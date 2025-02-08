ca5fd4b6a661169f502d7c3be39444f12ba0ade6
cilerba <maxdcordeiro@gmail.com>
2025-02-07 23:42:32 -0500

    Update Makefile to reference Python 3

--- ereader.mk
+++ ereader.mk
@@ -11,33 +11,33 @@ PROLOGUE_ASM	= prologue
 all: $(FILE_NAME).raw sav
 
 $(SCRIPT_ASM)-%.tx: $(SCRIPT_ASM).asm
-	python ../scripts/regionalize.py $< $@ $* $*
+	python3 ../scripts/regionalize.py $< $@ $* $*
 $(SCRIPT_ASM)-%.o: $(SCRIPT_ASM)-%.tx	
 	../tools/rgbasm -o $@ $<
 $(SCRIPT_ASM)-%.gbc: $(SCRIPT_ASM)-%.o
 	../tools/rgblink -o $@ $<
 $(SCRIPT_ASM)-%.bin: $(SCRIPT_ASM)-%.gbc
-	python ../scripts/stripgbc.py $< $@
+	python3 ../scripts/stripgbc.py $< $@
 $(SCRIPT_ASM)-%.mev: $(SCRIPT_ASM)-%.bin
-	python ../scripts/checksum.py $< $@
+	python3 ../scripts/checksum.py $< $@
 
 $(PROLOGUE_ASM)-%.tx: $(PROLOGUE_ASM).asm
-	python ../scripts/regionalize.py $< $@ $* $*
+	python3 ../scripts/regionalize.py $< $@ $* $*
 $(PROLOGUE_ASM)-%.o: $(PROLOGUE_ASM)-%.tx
 	../tools/rgbasm -o $@ $<
 $(PROLOGUE_ASM)-%.gbc: $(PROLOGUE_ASM)-%.o
 	../tools/rgblink -o $@ $<
 $(PROLOGUE_ASM)-%.bin: $(PROLOGUE_ASM)-%.gbc
-	python ../scripts/stripgbc.py $< $@
+	python3 ../scripts/stripgbc.py $< $@
 
 $(CARD_NAME)-%.tx: card.asm $(SCRIPT_ASM)-%.mev $(PROLOGUE_ASM)-%.bin
-	python ../scripts/ereadertext.py $< $@ $*
+	python3 ../scripts/ereadertext.py $< $@ $*
 $(CARD_NAME)-%.o: $(CARD_NAME)-%.tx
 	../tools/rgbasm -o $@ $<
 $(CARD_NAME)-%.gbc: $(CARD_NAME)-%.o
 	../tools/rgblink -o $@ $<
 $(CARD_NAME)-%.z80: $(CARD_NAME)-%.gbc
-	python ../scripts/stripgbc.py $< $@
+	python3 ../scripts/stripgbc.py $< $@
 $(CARD_NAME)-%.vpk: $(CARD_NAME)-%.z80
 	../tools/nevpk -c -i $< -o $@
 $(CARD_NAME)-%.raw: $(CARD_NAME)-%.vpk
